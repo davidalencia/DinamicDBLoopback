d0081493b98a9a47947269cc63d95cef1eb18cad
José G <jose.garcia@mecanica.mx>
2019-09-13 19:24:48 -0500

    cambio sencillo

---
+++ common/models/poblacion.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const RemoteDB = require('../utils/RemoteDB');
+
+module.exports = function(Poblacion) {
+  Poblacion.remoteMethod("agregaEnte", {
+    accepts: [
+      {
+        arg: "data",
+        type: "object",
+        required: true,
+        http: {
+          source: "body"
+        }
+      }
+    ],
+    http: {
+      path: "/agrega"
+    },
+    returns: {
+      type: "object",
+      root: true
+    }
+  });
+
+  Poblacion.agregaEnte = (data, cb) => {
+    const db = new RemoteDB({
+        host: 'localhost',
+        port: 1433,
+        database: 'tierra',
+        username: 'sa',
+        password: 'mmx2019.',
+    });
+    return db.save(Poblacion, data)
+    .then(res=>{
+      return res;
+    }).catch(err=>{
+      cb({
+        statusCode: 505,
+        message: "no pudo conectarse a db"
+      });
+    });
+  }
+
+};
---
+++ common/utils/RemoteDB.js
@@ -0,0 +1,27 @@
+const DataSource = require('loopback-datasource-juggler').DataSource;
+const connector = require('loopback-connector-mssql');
+
+
+
+class RemoteDB {
+  constructor(settings) {
+    this.settings = settings;
+    this.settings.connector = connector;
+  }
+
+  save(model, data){
+    const settings = this.settings
+    return new Promise(function(resolve, reject) {
+      const ds = new DataSource(settings);
+      ds.on('error', (err)=>{
+        reject(err);
+      });
+      ds.on('connected', ()=>{
+          model.attachTo(ds);
+          resolve(model.create(data));
+      });
+    });
+  }
+}
+
+module.exports = RemoteDB;
